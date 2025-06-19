5b655ac7434f39d5816a3df5ac56c4a6bf0b7a2b
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2025-06-19 11:57:14 +0200

    reverted agreement hide settings

--- src/config/default-app-config.ts
+++ src/config/default-app-config.ts
@@ -407,8 +407,8 @@ export class DefaultAppConfig implements AppConfig {
   // - A 404 page if you manually try to navigate to the privacy policy page at info/privacy
   // - All mentions of the privacy policy being removed from the UI (e.g. in the footer)
   info: InfoConfig = {
-    enableEndUserAgreement: false,
-    enablePrivacyStatement: false
+    enableEndUserAgreement: true,
+    enablePrivacyStatement: true
   };
 
   // Whether to enable Markdown (https://commonmark.org/) and MathJax (https://www.mathjax.org/)
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -295,8 +295,8 @@ export const environment: BuildConfig = {
     video: true
   },
   info: {
-    enableEndUserAgreement: false,
-    enablePrivacyStatement: false,
+    enableEndUserAgreement: true,
+    enablePrivacyStatement: true,
   },
   markdown: {
     enabled: false,
