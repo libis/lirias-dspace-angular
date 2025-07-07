32f958f762e27f4f4bcf94ec3d81ffa7654b478c
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2025-07-07 21:02:03 +0200

    local admins guard

--- src/app/core/data/feature-authorization/feature-authorization-guard/local-admin.guard.ts
+++ src/app/core/data/feature-authorization/feature-authorization-guard/local-admin.guard.ts
@@ -13,6 +13,13 @@ export class LocalAdminGuard implements CanActivate {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    return this.authService.getAuthenticatedUserFromStore()
+      .pipe(switchMap((user) => user.groups))
+      .pipe(getFirstSucceededRemoteData(), getRemoteDataPayload())
+      .pipe(
+        map((groups) => groups.page.map((g) => g.id === 'Admins_local')),
+        catchError(_ => [false]),
+        returnForbiddenUrlTreeOrLoginOnAllFalse(this.router, this.authService, state.url)
+      );
   }
 }
