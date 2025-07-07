2f7b2e94bd6270a240888369c64a538456c46211
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2025-07-07 23:34:54 +0200

    local admins guard

--- src/app/local-admin.guard.ts
+++ src/app/local-admin.guard.ts
@@ -7,7 +7,6 @@ import { Injectable } from '@angular/core';
 import { followLink } from 'src/app/shared/utils/follow-link-config.model';
 import { EPersonDataService } from 'src/app/core/eperson/eperson-data.service';
 import { AuthorizationDataService } from './core/data/feature-authorization/authorization-data.service';
-import { FeatureID } from './core/data/feature-authorization/feature-id';
 
 @Injectable({
   providedIn: 'root'
@@ -17,10 +16,6 @@ export class LocalAdminGuard implements CanActivate {
   }
 
   canActivate(_: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.authorizationService.isAuthorized(FeatureID.AdministratorOf, undefined, undefined, false)) {
-      console.log('authorized as administrator in local admin guard');
-      return true;
-    }
     return this.authService.getAuthenticatedUserFromStore()
       .pipe(switchMap((user) => this.epersonService.findById(user.id, true, true, followLink('groups'))))
       .pipe(getFirstSucceededRemoteData())
@@ -28,7 +23,7 @@ export class LocalAdminGuard implements CanActivate {
       .pipe(switchMap((user) => user.groups))
       .pipe(getFirstSucceededRemoteData())
       .pipe(getRemoteDataPayload())
-      .pipe(map((groups) => groups.page.map((g) => g.name === 'Admins_local')))
+      .pipe(map((groups) => groups.page.map((g) => 'Admins_local' === g.name || 'Administrator' === g.name)))
       .pipe(catchError(_ => [false]))
       .pipe(returnForbiddenUrlTreeOrLoginOnAllFalse(this.router, this.authService, state.url));
   }
