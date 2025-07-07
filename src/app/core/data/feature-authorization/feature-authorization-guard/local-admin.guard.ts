35d40fc86a476e7257847eb8faf17f6ec05a8a7b
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2025-07-07 20:09:25 +0200

    local admins guard

--- src/app/core/data/feature-authorization/feature-authorization-guard/local-admin.guard.ts
+++ src/app/core/data/feature-authorization/feature-authorization-guard/local-admin.guard.ts
@@ -5,14 +5,6 @@ import { map, Observable, switchMap} from 'rxjs';
 import { AuthService } from '../../../auth/auth.service';
 import { getFirstSucceededRemoteData, getRemoteDataPayload } from 'src/app/core/shared/operators';
 
-/**
- * Prevent unauthorized activating and loading of routes when the current authenticated user doesn't have administrator
- * rights to the {@link Site}
- */
-@Injectable({
-  providedIn: 'root'
-})
-
 export class LocalAdminGuard implements CanActivate {
   constructor(protected authorizationService: AuthorizationDataService, protected router: Router, protected authService: AuthService) {
   }
